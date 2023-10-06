880e22aa6cb800f8c7d9c394515c6cf8c9114297
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-05 22:24:52 -0700

    FIX: Added commands to telescope and added ignore patterns

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,18 @@
 local builtin = require('telescope.builtin')
+
+-- Fuzzy finder and buffers
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+-- Display command history
+vim.keymap.set('n', '<leader>ch', builtin.command_history, {})
+
+-- Change color scheme
+vim.keymap.set('n', '<leader>clr', builtin.colorscheme, {})
+
+require('telescope').setup{
+	defaults = {
+		file_ignore_patterns = { "node_modules" }
+	},
+	extensions = {}
+}
