81e0c6c88203d3dff32d47954f3a3f5f6817fdc0
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-25 23:58:47 -0700

    FEAT: Toggle Auto-Formatting for GO

--- after/plugin/formatting.lua
+++ after/plugin/formatting.lua
@@ -14,6 +14,25 @@ vim.g["prettier#config#config_files"] = {
 vim.keymap.set('n', '<leader>p', ':PrettierAsync<CR>')
 
 -- GO (gfmt)
-vim.api.nvim_exec([[
-  autocmd FileType go autocmd BufWritePre <buffer> :silent %!gofmt -s
-]], false)
+local autoformat_enabled = false
+
+-- Function to toggle auto-formatting on/off
+function toggle_autoformat()
+    autoformat_enabled = not autoformat_enabled
+    if autoformat_enabled then
+        vim.api.nvim_out_write("GO Auto-formatting enabled\n")
+        vim.api.nvim_exec([[
+            augroup GoAutoformat
+                autocmd!
+                autocmd FileType go autocmd BufWritePre <buffer> :silent %!gofmt -s
+            augroup END
+        ]], true)
+    else
+        vim.api.nvim_out_write("GO Auto-formatting disabled\n")
+        vim.cmd("autocmd! GoAutoformat")
+    end
+end
+
+-- Map a keybind to toggle auto-formatting
+vim.api.nvim_set_keymap('n', '<leader>af', ':lua toggle_autoformat()<CR>', { noremap = true, silent = true })
+
