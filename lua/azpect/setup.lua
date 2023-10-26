3db2c006736f6e4112f4046477aeb50939d168d2
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-25 21:03:49 -0700

    FIX: Removed folding, sucky stuff

--- lua/azpect/setup.lua
+++ lua/azpect/setup.lua
@@ -44,6 +44,6 @@ vim.cmd([[highlight SpellLocal cterm=none gui=none]])
 vim.cmd('autocmd BufNewFile,BufRead *.ejs setfiletype html')
 
 -- Set up folding
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.keymap.set('n', '<leader><leader>', 'za')
+-- vim.opt.foldmethod = 'expr'
+-- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+-- vim.keymap.set('n', '<leader><leader>', 'za')
