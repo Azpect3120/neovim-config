9c2ffcbbc3f0256a39553b4cfeded6d06b3d9f9e
Azpect3120 <hhargreaves2006@gmail.com>
2023-10-26 11:19:56 -0700

    FEAT: Added formatting with _p

--- after/plugin/formatting.lua
+++ after/plugin/formatting.lua
@@ -10,29 +10,14 @@ vim.g["prettier#config#config_files"] = {
   ".prettierrc.cjs",
 }
 
--- Prettier remap
-vim.keymap.set('n', '<leader>p', ':PrettierAsync<CR>')
-
--- GO (gfmt)
-local autoformat_enabled = false
-
--- Function to toggle auto-formatting on/off
-function toggle_autoformat()
-    autoformat_enabled = not autoformat_enabled
-    if autoformat_enabled then
-        vim.api.nvim_out_write("GO Auto-formatting enabled\n")
-        vim.api.nvim_exec([[
-            augroup GoAutoformat
-                autocmd!
-                autocmd FileType go autocmd BufWritePre <buffer> :silent %!gofmt -s
-            augroup END
-        ]], true)
-    else
-        vim.api.nvim_out_write("GO Auto-formatting disabled\n")
-        vim.cmd("autocmd! GoAutoformat")
-    end
+function format_buffer ()
+  local filetype = vim.bo.filetype
+  if filetype == "go" then
+    vim.api.nvim_command("%!gofmt -s")
+  else
+    vim.api.nvim_command(":PrettierAsync")
+  end
 end
 
--- Map a keybind to toggle auto-formatting
-vim.api.nvim_set_keymap('n', '<leader>af', ':lua toggle_autoformat()<CR>', { noremap = true, silent = true })
-
+-- Keybind
+vim.keymap.set('n', '<leader>p', ':lua format_buffer()<CR>')
