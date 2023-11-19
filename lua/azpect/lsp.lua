48eb4a4afc45644ce6d1de88bc6f2b8200400b0d
Azpect3120 <hhargreaves2006@gmail.com>
2023-11-18 23:24:48 -0700

    Added bash lsp

--- lua/azpect/lsp.lua
+++ lua/azpect/lsp.lua
@@ -27,7 +27,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     }
 )
 
-local servers = { 'tailwindcss', 'html', 'cssls', 'tsserver', 'clangd', 'gopls', 'rust_analyzer', 'htmx', 'jdtls', 'pylsp' }
+local servers = { 'tailwindcss', 'html', 'cssls', 'tsserver', 'clangd', 'gopls', 'rust_analyzer', 'htmx', 'jdtls', 'pylsp', 'bashls' }
 
 lspconfig.tsserver.setup { on_attach = on_attach }
 
