29316c8194efa4cb850c138c6f823afbc11f861e
Azpect3120 <hhargreaves2006@gmail.com>
2023-12-13 23:11:00 -0700

    FEAT: Added '_rn' remap for lsp renaming.

--- lua/azpect/lsp.lua
+++ lua/azpect/lsp.lua
@@ -64,3 +64,17 @@ cmp.setup({
         { name = 'tailwindcss' },
     },
 })
+
+-- Default config
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    local opts = { buffer = ev.buf }
+    -- Rename remap
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+  end,
+})
